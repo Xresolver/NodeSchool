d42833b09dde15588b9137359df56db08d5c4c1b
Seyit Bilal <sbilalcelik@gmail.com>
2022-04-02 06:24:45 +0300

    Remove Spaces

--- 09-functional-javascript-workshop/07-basic_recursion.js
+++ 09-functional-javascript-workshop/07-basic_recursion.js
@@ -7,10 +7,7 @@ function reduce(arr, fn, initial)
         initial = fn(initial, arr[0])
         return reduce(arr.slice(1), fn, initial)
     }
-        
-
 }
-
 module.exports = reduce
 
 /* Official Solution */
