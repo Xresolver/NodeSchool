86a1bd82d4cde81d1265d5b1de167ff297c8716f
Seyit Bilal Çelik <Xresolver@users.noreply.github.com>
2022-11-25 17:29:40 +0300

    add short array filtering

--- 01-javascripting/12-array-filtering.js
+++ 01-javascripting/12-array-filtering.js
@@ -1,10 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const filtered = numbers.filter
-  (
-    function evenNumbers (number)
-    {
-       return number % 2 === 0
-    }
-  );
-
+const filtered = numbers.filter((number) => number % 2 === 0);
 console.log(filtered);
+
+// const filtered = numbers.filter( 
+//   function evenNumbers (number)
+//     { return number % 2 === 0 }
+//   );
