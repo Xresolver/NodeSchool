839d1c7d916c8a8d505cbbca783fd75aa0792dfa
Seyit Bilal Çelik <Xresolver@users.noreply.github.com>
2023-11-28 15:12:29 +0300

    Some extra solution and tests

--- 09-functional-javascript-workshop/02-higher-order-functions.js
+++ 09-functional-javascript-workshop/02-higher-order-functions.js
@@ -1,8 +1,15 @@
-function repeat(operation, num) {
-    if(num > 0)
-        repeat(operation, num -1)
-    return operation
-  }
+// function repeat(operation, num) {
+//     if(num > 0)
+//         repeat(operation, num -1)
+//     return operation
+//   }
+
+function repeat(operation, num)
+{
+    for(let i = 0; i < num; i++)
+        operation();
+}
 
   // Do not remove the line below
   module.exports = repeat
+
