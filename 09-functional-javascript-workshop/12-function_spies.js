07a7a6372f435964a874009d480dd0f2cb3de8de
Xresolver <sbilalcelik@gmail.com>
2023-12-12 03:37:00 +0300

    12 - Function Spies

--- 09-functional-javascript-workshop/12-function_spies.js
+++ 09-functional-javascript-workshop/12-function_spies.js
@@ -1,7 +1,36 @@
-function Spy(target, method) {
-    const obj = {count:0};
-    target.method.bind(obj)
-    
-  }
+function Spy(target, method) 
+{
+    var func = target[method]
 
-  module.exports = Spy
+    const obj = {
+        count: 0,
+        method: function(){
+            obj.count++;
+            return func.call(target,...arguments);
+        }
+    }
+
+    target[method] = obj.method
+    return obj;
+}
+
+/* OFFICIEL SOLUTION */
+/* ----------------- */
+// function Spy(target, method) {
+//     var originalFunction = target[method]
+
+//     // use an object so we can pass by reference, not value
+//     // i.e. we can return result, but update count from this scope
+//     var result = {
+//       count: 0
+//     }
+
+//     // replace method with spy method
+//     target[method] = function() {
+//       result.count++ // track function was called
+//       return originalFunction.apply(this, arguments) // invoke original function
+//     }
+
+//     return result
+//   }
+// module.exports = Spy
