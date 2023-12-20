24796b1350a421f4469c55b020f03e4abf6951d6
Xresolver <sbilalcelik@gmail.com>
2023-12-20 22:49:41 +0300

    17-currying.js

---
+++ 09-functional-javascript-workshop/17-currying.js
@@ -0,0 +1,32 @@
+function curryN(fn, n) 
+{
+    let fnNumber = 0
+    if(n !== undefined)
+        fnNumber = n
+    else
+        fnNumber = fn.length;
+
+
+    if(fnNumber > 0)
+    {   
+        if(arguments[2])
+            fn = fn.bind(null, arguments[2]) 
+        return curryN.bind(null, fn, --fnNumber)
+    }
+    else
+        return fn.apply(null,[arguments[2]]);
+
+}
+
+module.exports = curryN
+
+/* OFFICIEL SOLUTION */
+// function curryN(fn, n) {
+//     n = n || fn.length
+//     return function curriedN(arg) {
+//       if (n <= 1) return fn(arg)
+//       return curryN(fn.bind(this, arg), n - 1)
+//     }
+//   }
+
+//   module.exports = curryN
