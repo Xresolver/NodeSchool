e82dd6fa92cb04a76c226489af7c862711db6a69
Xresolver <sbilalcelik@gmail.com>
2024-01-05 00:27:16 +0300

    makemehapi-directories

---
+++ 12-makemehapi/04-directories.js
@@ -0,0 +1,70 @@
+const hapi = require('@hapi/hapi');
+const inert = require('@hapi/inert');
+const path = require('path');
+
+(async () => {
+    try 
+    {
+        const server = hapi.Server({
+            host: 'localhost',
+            port: Number(process.argv[2] || 8080),
+            routes: {
+                files: {
+                    relativeTo: path.join(__dirname, 'public')
+                }
+            }
+        
+        });
+        
+        await server.register(inert);
+    
+        server.route({
+            method: 'GET',
+            path: '/{param1}/{param2}/{param3}/{param*}',
+            handler: {
+                directory: {
+                    path: '.',
+                }
+            }
+        });
+    
+        await server.start();
+    
+        console.log('Server running at:', server.info.uri);
+    } 
+    catch (error) 
+    {
+        console.log(error);
+    }
+})();
+
+/* OFFICIEL SOLUTION */
+/* ----------------- */
+// const Path = require('path');
+// const Hapi = require('@hapi/hapi');
+// const Inert = require('@hapi/inert');
+
+// (async () => {
+//   try {
+//     const server = Hapi.Server({
+//       host: 'localhost',
+//       port: process.argv[2] || 8080
+//     });
+
+//     await server.register(Inert);
+
+//     server.route({
+//       path: '/foo/bar/baz/{filename}',
+//       method: 'GET',
+//       handler: {
+//         directory: {
+//           path: Path.join(__dirname, 'public')
+//         }
+//       }
+//     });
+
+//     await server.start();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// })();
