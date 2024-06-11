fcbf54318b748f875a543b4e34fd0f28647107c1
Xresolver <sbilalcelik@gmail.com>
2024-06-11 10:55:44 +0300

    5. Exercise Amendment

--- 29-shader-school/04-intro-4/box.glsl
+++ 29-shader-school/04-intro-4/box.glsl
@@ -1,27 +1,24 @@
 bool inBox(highp vec2 lo, highp vec2 hi, highp vec2 p) {
 
   //Test if the point p is inside the box bounded by [lo, hi]
-  bool lo_x_gt_hi_x = lo.x > hi.x;
-  bool lo_y_gt_hi_y = lo.y > hi.x;
-  bool lo_x_gt_p_x = lo.x > p.x;
-  bool lo_y_gt_p_y = lo.y > p.y;
-  bool hi_x_gt_p_x = hi.x > p.x;
-  bool hi_y_gt_p_y = hi.y > p.y;
+  bvec2 loGThi = greaterThan(lo, hi);
+  bvec2 loGTp = greaterThan(lo, p);
+  bvec2 hiGTp = greaterThan(hi, p);
 
-  if(lo_x_gt_hi_x && lo_x_gt_p_x && !hi_x_gt_p_x)
+  if(loGThi.x && loGTp.x && !hiGTp.x)
   {
-    if(lo_y_gt_hi_y && lo_y_gt_p_y && !hi_y_gt_p_y)
+    if(loGThi.y && loGTp.y && !hiGTp.y)
       return true;
-    else if (!lo_y_gt_hi_y && !lo_y_gt_p_y && hi_y_gt_p_y)
+    else if (!loGThi.y && !loGTp.y && hiGTp.y)
       return true;
     else
       return false;
   }
-  else if(!lo_x_gt_hi_x && !lo_x_gt_p_x && hi_x_gt_p_x)
+  else if(!loGThi.x && !loGTp.x && hiGTp.x)
   {
-    if(lo_y_gt_hi_y && lo_y_gt_p_y && !hi_y_gt_p_y)
+    if(loGThi.y && loGTp.y && !hiGTp.y)
       return true;
-    else if (!lo_y_gt_hi_y && !lo_y_gt_p_y && hi_y_gt_p_y)
+    else if (!loGThi.y && !loGTp.y && hiGTp.y)
       return true;
     else
       return false;
