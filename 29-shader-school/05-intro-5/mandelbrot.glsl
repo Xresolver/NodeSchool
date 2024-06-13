348c1f20b21dcb245bdcf0b3f5ab25d8d0d7543b
Xresolver <sbilalcelik@gmail.com>
2024-06-13 17:15:32 +0300

    06-intro-6 Matrices for Shader School

--- 29-shader-school/05-intro-5/mandelbrot.glsl
+++ 29-shader-school/05-intro-5/mandelbrot.glsl
@@ -1,11 +1,26 @@
+vec2 mandelbrot(vec2 z, vec2 c) {
+  return vec2(z.x * z.x - z.y * z.y, 2.0 * z.x * z.y) + c;
+}
+
+bool mandelbrotConverges(vec2 z) {
+  return (length(z) < 2.0);
+}
+
 bool mandelbrot(highp vec2 c) {
 
   //Test if the point c is inside the mandelbrot set after 100 iterations
-  vec2 z = vec2(0.0);
+  vec2 z = vec2(0.0, 0.0);
+  bool inside;
 
-  return false;
+  for(int i = 0; i < 100; i++)
+  {
+    z = mandelbrot(z, c);
+    inside = mandelbrotConverges(z);
+  }
+
+  return mandelbrotConverges(z);
+
 }
-
 
 //Do not change this line or the name of the above function
 #pragma glslify: export(mandelbrot)
