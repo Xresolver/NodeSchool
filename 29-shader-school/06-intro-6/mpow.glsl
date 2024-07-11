cbd422171fb920b5aafabc0fe42b776a743aac00
Xresolver <sbilalcelik@gmail.com>
2024-07-11 11:37:18 +0300

    Getting Started - Matrices Completed

--- 29-shader-school/06-intro-6/mpow.glsl
+++ 29-shader-school/06-intro-6/mpow.glsl
@@ -7,7 +7,17 @@ mat2 matrixPower(highp mat2 m, int n) {
   //  matrixPower(m, 2) = m * m
   //
 
-  return mat2(1.0);  
+  highp mat2 result = mat2(1.0);
+  
+  for(int i = 0; i < 16; i++)
+  {
+    if(i == n)
+      break;
+
+    result = result * m;
+  }
+
+  return result;  
 }
 
 //Do not change this line or the name of the above function
