9c5df4e35aefac0bad13ab7b5ddbd49380e58da8
Xresolver <sbilalcelik@gmail.com>
2024-07-20 14:30:05 +0300

    Fragment Shaders - The Basics

--- 29-shader-school/07-frag-1/fragment.glsl
+++ 29-shader-school/07-frag-1/fragment.glsl
@@ -3,13 +3,16 @@ precision highp float;
 #define CIRCLE_COLOR    vec4(1.0, 0.4313, 0.3411, 1.0)
 #define OUTSIDE_COLOR   vec4(0.3804, 0.7647, 1.0, 1.0)
 
-void main() {
-
+void main() 
+{
   //TODO: Replace this with a function that draws a circle at (256.5,256.5) with radius 128
-  
-  if(gl_FragCoord.y > 256.0) {
+  vec2 center = vec2(256, 256);
+
+  float dist = distance(gl_FragCoord.xy, center);
+
+  if(dist < 128.0) 
     gl_FragColor = CIRCLE_COLOR;
-  } else {
+  else 
     gl_FragColor = OUTSIDE_COLOR;
-  }
+  
 }
