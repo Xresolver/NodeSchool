c76a655968d8a53bdb88947ed75414f05702fb51
Xresolver <sbilalcelik@gmail.com>
2024-07-22 16:14:56 +0300

    Fragment Shader - Uniforms and Textures

--- 29-shader-school/09-frag-3/fragment.glsl
+++ 29-shader-school/09-frag-3/fragment.glsl
@@ -7,6 +7,8 @@ void main() {
   vec2 coord = gl_FragCoord.xy / screenSize;
 
   //TODO: Swap red and blue color channels of image
+  vec4 color = texture2D(texture, coord);
+  vec4 swappedColor = vec4(color.z, color.y, color.x, color.w);
 
-  gl_FragColor = texture2D(texture, coord);
+  gl_FragColor = swappedColor;
 }
