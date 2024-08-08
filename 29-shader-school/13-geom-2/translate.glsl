af97b226043645af4127315aa5f56682a7dfaf8a
Xresolver <sbilalcelik@gmail.com>
2024-08-08 16:37:03 +0300

    Geometry - Clip Coordinates Geometry - Translations

--- 29-shader-school/13-geom-2/translate.glsl
+++ 29-shader-school/13-geom-2/translate.glsl
@@ -1,11 +1,13 @@
 highp mat4 translate(highp vec3 p) {
 
   //TODO: Construct a matrix, m, which translates all points so that p is at the origin.
+  mat4 m =  mat4(1, 0, 0, 0,
+                 0, 1, 0, 0, 
+                 0, 0, 1, 0,
+                 -p.x, -p.y, -p.z, 1);
 
-  return mat4(1, 0, 0, 0,
-              0, 1, 0, 0, 
-              0, 0, 1, 0,
-              0, 0, 0, 1);
+
+  return m;
 }
 
 //Do not remove this line
