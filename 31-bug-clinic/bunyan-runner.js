ee71b710eb8ced86af2329869f7bae9acf1a661a
Xresolver <sbilalcelik@gmail.com>
2024-08-23 16:45:47 +0300

    bug clinic 4- Monitor

---
+++ 31-bug-clinic/bunyan-runner.js
@@ -0,0 +1,9 @@
+var resolve = require("path").resolve;
+
+var bunyan = require("bunyan");
+var log = bunyan.createLogger({name : "scenario"});
+
+var scenario = require(resolve(process.cwd(), process.argv[2]));
+scenario(log, function (value) {
+  log.info("value at finish is", value);
+});
